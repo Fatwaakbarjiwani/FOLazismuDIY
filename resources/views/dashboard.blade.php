a69e15390cc6b99225a153794b89d39785b50a66
32602100045_Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2025-01-06 18:02:57 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,27 @@
 </head>
 
 <body class="bg-gray-100 flex justify-center">
-    @include('components.loading')
+    <div class="bg-white flex justify-center items-center min-h-screen">
+    <!-- Wrapper dengan maksimal lebar 500px -->
+    <div id="loadingScreen"
+        class="relative w-full md:w-[500px] h-screen flex flex-col justify-center items-center bg-white">
+        <!-- Logo dengan animasi pulse -->
+        <img alt="Lazismu Logo" src="{{ asset('image/logoOrange.png') }}" class="h-32 animate-pulse" />
+
+        <!-- Teks dengan efek mengetik dan gradasi -->
+        <div class="mt-1 text-center">
+            <span
+                class="block bg-clip-text text-transparent bg-gradient-to-r from-[#FFA500] via-[#FF4500] to-[#FF6347] text-lg sm:text-xl font-semibold">
+                SIAPA TAHU,
+            </span>
+            <span
+                class="block bg-clip-text text-transparent bg-gradient-to-r from-[#FFA500] via-[#FF4500] to-[#FF6347] text-lg sm:text-xl font-semibold">
+                INI KEBAIKANMU UNTUK YANG TERAKHIR KALINYA
+            </span>
+        </div>
+    </div>
+</div>
+
 
     <div id="dashboardContent" class="hidden w-full md:w-[500px] bg-white shadow-md">
         @include('components.header')
