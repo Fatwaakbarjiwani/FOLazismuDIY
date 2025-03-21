196ed5cc67348cb2dbfb8b73c28621d099ac0336
Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2025-03-21 13:50:01 +0700

    update

--- resources/views/formPembayaran2.blade.php
+++ resources/views/formPembayaran2.blade.php
@@ -4,34 +4,46 @@
 <head>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-KWQ5Z1NW94"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
 
-    gtag('config', 'G-KWQ5Z1NW94');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-KWQ5Z1NW94');
     </script>
     <meta charset="utf-8" />
     <title>Payment Page</title>
     <!-- Meta Pixel Code -->
     <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '6779218792190886');
-    fbq('track', 'PageView');
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '6779218792190886');
+        fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=6779218792190886&ev=PageView&noscript=1"
-    /></noscript>
+            src="https://www.facebook.com/tr?id=6779218792190886&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/svg+xml" href="image/logoOrange.png"/>
+    <link rel="icon" type="image/svg+xml" href="image/logoOrange.png" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
@@ -207,17 +219,7 @@
                 .then(response => response.json()) // Parse the response as JSON
                 .then(data => {
                     localStorage.setItem('Ct', data.created_time); // Simpan waktu transaksi ke localStorage
-
-                    // Tampilkan pesan sukses dengan SweetAlert
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Lanjutkan Pembayaran!',
-                        text: 'Silakan lanjutkan ke halaman QRIS.',
-                        showConfirmButton: false,
-                        timer: 2000,
-                    }).then(() => {
-                        window.location.href = 'qris_ziska'; // Redirect setelah sukses
-                    });
+                    window.location.href = 'qris_ziska'; // Redirect setelah sukses
                 })
                 .catch((error) => {
                     console.error('Error:', error);
